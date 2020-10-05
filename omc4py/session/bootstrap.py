26b15749a1c170ce9d271c1af6d13cf443030dd5
ijknabla <ijknabla@gmail.com>
2020-10-05 20:38:46 +0900

    Add with_errorcheck decorator

--- omc4py/session/bootstrap.py
+++ omc4py/session/bootstrap.py
@@ -1,20 +1,57 @@
 
 from .. import parsers
 
+import functools
+import typing
+
 from . import (
     StrOrPathLike,
     InteractiveOMC,
 )
 
 
+class OMCError(
+    Exception
+):
+    ...
+
+
+def with_errorcheck(
+    func: typing.Callable
+) -> typing.Callable:
+    @functools.wraps(func)
+    def wrapped(
+        omc: InteractiveOMC,
+        *args,
+        check: bool = True,
+        **kwrds,
+    ):
+        result = func(omc, *args, **kwrds)
+        errorstring = omc.execute("getErrorString").rstrip()
+        if errorstring:
+            raise OMCError(errorstring)
+        return result
+
+    return wrapped
+
+
+@with_errorcheck
+def execute(
+    omc: InteractiveOMC,
+    expression: str,
+):
+    return omc.execute(expression)
+
+
 def bootstrap(
     omc_command: StrOrPathLike
 ):
     with InteractiveOMC.open(omc_command) as omc:
-        component_record_decl = omc.execute(
+        component_record_decl = execute(
+            omc,
             "getComponentsTest("
             "    OpenModelica.Scripting.getComponentsTest.Component"
-            ")"
+            ")",
         )
 
     tree = parsers.omc_value_parser.parse(component_record_decl)
