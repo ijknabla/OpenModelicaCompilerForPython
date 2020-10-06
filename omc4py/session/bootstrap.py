c8ca8287877460950c509776087404ba3f7900cb
ijknabla <ijknabla@gmail.com>
2020-10-06 12:21:52 +0900

    Implement getComponentsTest for code inspection

--- omc4py/session/bootstrap.py
+++ omc4py/session/bootstrap.py
@@ -1,6 +1,7 @@
 
 from .. import parsers
 
+import arpeggio
 import enum
 import functools
 import operator
@@ -64,16 +65,101 @@ def execute(
     return omc.execute(expression)
 
 
+class IdentifierVisitor(
+    arpeggio.PTNodeVisitor,
+):
+    identifierType: typing.Type
+
+    def __init__(
+        self,
+        *args,
+        identifierType: typing.Type = str,
+        **kwrds,
+    ):
+        super().__init__(*args, **kwrds)
+        self.identifierType = identifierType
+
+    def visit_IDENT(self, node, *_):
+        return self.identifierType(node.value)
+
+
+class Component(
+    typing.NamedTuple
+):
+    """
+OpenModelica.Scripting.getComponentsTest.Component
+"""
+    className: str
+    name: str
+    comment: str
+    isProtected: bool
+    isFinal: bool
+    isFlow: bool
+    isStream: bool
+    isReplaceable: bool
+    variability: str
+    innerOuter: str
+    inputOutput: str
+    dimensions: typing.Tuple[str]
+
+
+class GetComponentsTestVisitor(
+    IdentifierVisitor,
+    parsers.visitor.StringVisitor,
+    parsers.visitor.BooleanVisitor,
+    parsers.visitor.SequenceVisitor,
+):
+    def visit_omc_record_element(
+        self,
+        node,
+        children,
+    ):
+        IDENT = children.IDENT[0]
+        value = children.omc_value[0]
+        if isinstance(value, typing.List):
+            value = tuple(value)
+        return IDENT, value
+
+    def visit_omc_record_element_list(
+        self,
+        node,
+        children,
+    ):
+        return children.omc_record_element
+
+    def visit_omc_record_literal(
+        self,
+        node,
+        children,
+    ):
+        return Component(**dict(children.omc_record_element_list[0]))
+
+
+@with_errorcheck
+def getComponents(
+    omc: InteractiveOMC,
+    class_: str,
+) -> typing.Tuple[Component, ...]:
+    literal = omc.execute(
+        f"getComponentsTest({class_})",
+    )
+    result = arpeggio.visit_parse_tree(
+        parsers.omc_value_parser.parse(literal),
+        GetComponentsTestVisitor(),
+    )
+    return tuple(result)
+
+
 def bootstrap(
     omc_command: StrOrPathLike
 ):
     with InteractiveOMC.open(omc_command) as omc:
-        component_record_decl = execute(
+        for component in getComponents(
             omc,
-            "getComponentsTest("
-            "    OpenModelica.Scripting.getComponentsTest.Component"
-            ")",
-        )
+            "OpenModelica.Scripting.getComponentsTest.Component"
+        ):
+            print(component)
 
-        tree = parsers.omc_value_parser.parse(component_record_decl)
-    print(tree)
+
+if __name__ == "__main__":
+    bootstrap("omc")
