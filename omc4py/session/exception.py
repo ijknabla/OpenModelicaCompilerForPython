ad17dcdd620378c8a6ae90cd98b4faecbd010d87
ijknabla <ijknabla@gmail.com>
2020-10-12 11:46:58 +0900

    Implement parse_omc_error_message

--- omc4py/session/exception.py
+++ omc4py/session/exception.py
@@ -5,6 +5,9 @@ __all__ = (
     "OMCWarning",
 )
 
+import re
+import typing
+
 
 class OMCException(
     Exception,
@@ -23,3 +26,43 @@ class OMCError(
     OMCException,
 ):
     ...
+
+
+class OMCRuntimeError(
+    OMCError,
+    RuntimeError,
+):
+    ...
+
+
+omc_error_pattern = re.compile(
+    r"\[(?P<info>[^]]*)\]\s+(?P<kind>\w+):\s+(?P<message>.*)"
+)
+
+
+def parse_omc_error_message(
+    error_message: str
+) -> typing.Optional[OMCException]:
+    error_message = error_message.rstrip()
+    if not error_message:
+        return None
+
+    matched = omc_error_pattern.match(
+        error_message
+    )
+    if not matched:
+        raise OMCRuntimeError(
+            f"Unexpected error message format: {error_message!r}"
+        )
+    # info = matched.group("info")
+    kind = matched.group("kind")
+    # message = matched.group("message")
+
+    if kind == "Error":
+        return OMCError(error_message)
+    elif kind == "Warning":
+        return OMCWarning(error_message)
+    else:
+        raise NotImplementedError(
+            "Unexpected omc error kind: {kind!r}"
+        )
