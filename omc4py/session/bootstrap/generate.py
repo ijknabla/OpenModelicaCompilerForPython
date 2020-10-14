f0570418ef2436c3a688fdd2aabed038ae81105a
ijknabla <ijknabla@gmail.com>
2020-10-14 22:55:07 +0900

    Implement encode_typeName

--- omc4py/session/bootstrap/generate.py
+++ omc4py/session/bootstrap/generate.py
@@ -1,11 +1,33 @@
 
 import argparse
-from lxml import etree as xml
+from lxml import etree as xml  # type: ignore
 from pathlib import Path
+import re
+import typing
 
 from . import (
-    load_schema
+    load_schema,
 )
+
+from .. import types
+
+
+def encode_typeName(
+    typeName: types.TypeName,
+    encoding: str = "utf-8",
+) -> str:
+    pattern = re.compile(r"[0-9A-Za-z]")
+
+    def characters(
+    ) -> typing.Iterator[str]:
+        for char in str(typeName):
+            if pattern.match(char):
+                yield char
+            else:
+                code_point = ord(char)
+                yield f"_{code_point:x}_"
+
+    return "".join(characters())
 
 
 def main():
