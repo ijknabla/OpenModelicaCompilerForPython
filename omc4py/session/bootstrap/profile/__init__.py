872931a3efc5ca2ce5a256f5c7743a41acddb836
ijknabla <ijknabla@gmail.com>
2020-10-19 19:39:57 +0900

    Move AbstractProfile class

--- omc4py/session/bootstrap/profile/__init__.py
+++ omc4py/session/bootstrap/profile/__init__.py
@@ -15,6 +15,10 @@ from omc4py.session import (
     types,
 )
 
+from .base import (
+    AbstractProfile,
+)
+
 
 def to_pyVariableName(
     variableName: types.VariableName,
@@ -23,66 +27,6 @@ def to_pyVariableName(
     while keyword.iskeyword(result):
         result += "_"
     return result
-
-
-class AbstractProfile(
-    abc.ABC
-):
-    def __init__(
-        self,
-        root: xml._Element,
-        name: types.TypeName,
-    ):
-        self.root = root
-        self.name = name
-
-    def __hash__(
-        self
-    ):
-        return hash(
-            tuple(map(hash, [self.root, self.name]))
-        )
-
-    def __eq__(
-        self,
-        other
-    ):
-        if not isinstance(other, AbstractProfile):
-            return False
-        else:
-            return (
-                self.root == other.root
-                and self.name == other.name
-            )
-
-    @classmethod
-    @abc.abstractmethod
-    def match(
-        cls,
-        root: xml._Element,
-        name: types.TypeName,
-    ) -> bool:
-        return False
-
-    @property
-    @abc.abstractmethod
-    def supported(
-        self
-    ) -> bool:
-        return False
-
-    @staticmethod
-    def find_element(
-        root: xml._Element,
-        name: types.TypeName,
-    ) -> typing.Optional[xml._Element]:
-        element_list = root.xpath(
-            f'//*[@id="{name!s}"]'
-        )
-        if element_list:
-            return element_list[0]
-        else:
-            return None
 
 
 class ExtrinsicProfile(
---
+++ omc4py/session/bootstrap/profile/base.py
@@ -0,0 +1,66 @@
+
+import abc
+from lxml import etree as xml
+import typing
+
+from omc4py.session import types
+
+
+class AbstractProfile(
+    abc.ABC
+):
+    def __init__(
+        self,
+        root: xml._Element,
+        name: types.TypeName,
+    ):
+        self.root = root
+        self.name = name
+
+    def __hash__(
+        self
+    ):
+        return hash(
+            tuple(map(hash, [self.root, self.name]))
+        )
+
+    def __eq__(
+        self,
+        other
+    ):
+        if not isinstance(other, AbstractProfile):
+            return False
+        else:
+            return (
+                self.root == other.root
+                and self.name == other.name
+            )
+
+    @classmethod
+    @abc.abstractmethod
+    def match(
+        cls,
+        root: xml._Element,
+        name: types.TypeName,
+    ) -> bool:
+        return False
+
+    @property
+    @abc.abstractmethod
+    def supported(
+        self
+    ) -> bool:
+        return False
+
+    @staticmethod
+    def find_element(
+        root: xml._Element,
+        name: types.TypeName,
+    ) -> typing.Optional[xml._Element]:
+        element_list = root.xpath(
+            f'//*[@id="{name!s}"]'
+        )
+        if element_list:
+            return element_list[0]
+        else:
+            return None
