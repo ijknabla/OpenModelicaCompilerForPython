1e12182287944f75a90990c74c17a76a8f41373c
ijknabla <ijknabla@gmail.com>
2020-10-21 14:04:45 +0900

    Implement supported

--- omc4py/session/bootstrap/profile/type_declaration.py
+++ omc4py/session/bootstrap/profile/type_declaration.py
@@ -15,6 +15,7 @@ class IntrinsicTypeConfig(
     typing.NamedTuple,
 ):
     primitive: bool
+    supported: bool
 
 
 IntrinsicTypeConfigs = typing.Dict[
@@ -25,30 +26,37 @@ _intrinsicTypeConfigs: IntrinsicTypeConfigs = {
     TypeName("Real"):
         IntrinsicTypeConfig(
             primitive=True,
+            supported=True,
         ),
     TypeName("Integer"):
         IntrinsicTypeConfig(
             primitive=True,
+            supported=True,
         ),
     TypeName("Boolean"):
         IntrinsicTypeConfig(
             primitive=True,
+            supported=True,
         ),
     TypeName("String"):
         IntrinsicTypeConfig(
             primitive=True,
+            supported=True,
         ),
     TypeName("OpenModelica.$Code.VariableName"):
         IntrinsicTypeConfig(
             primitive=False,
+            supported=True,
         ),
     TypeName("OpenModelica.$Code.VariableNames"):
         IntrinsicTypeConfig(
             primitive=False,
+            supported=False,
         ),
     TypeName("OpenModelica.$Code.TypeName"):
         IntrinsicTypeConfig(
             primitive=False,
+            supported=True,
         ),
 }
 
@@ -64,3 +72,15 @@ class IntrinsicTypeProfile(
         name: TypeName,
     ) -> bool:
         return name in _intrinsicTypeConfigs
+
+    @property
+    def config(
+        self,
+    ) -> IntrinsicTypeConfig:
+        return _intrinsicTypeConfigs[self.name]
+
+    @property
+    def supported(
+        self,
+    ) -> bool:
+        return self.config.supported
