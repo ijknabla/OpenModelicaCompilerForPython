56edadde526ff61cdaed37e1641586e5d76ee223
ijknabla <ijknabla@gmail.com>
2020-10-21 13:36:41 +0900

    Implement IntrinsicTypeProfile

---
+++ omc4py/session/bootstrap/profile/types.py
@@ -0,0 +1,66 @@
+
+from lxml import etree as xml  # type: ignore
+import typing
+
+from omc4py.session.types import (
+    TypeName
+)
+
+from . base import (
+    AbstractTypeProfile,
+)
+
+
+class IntrinsicTypeConfig(
+    typing.NamedTuple,
+):
+    primitive: bool
+
+
+IntrinsicTypeConfigs = typing.Dict[
+    TypeName, IntrinsicTypeConfig
+]
+
+__intrinsicTypeConfigs: IntrinsicTypeConfigs = {
+    TypeName("Real"):
+        IntrinsicTypeConfig(
+            primitive=True,
+        ),
+    TypeName("Integer"):
+        IntrinsicTypeConfig(
+            primitive=True,
+        ),
+    TypeName("Boolean"):
+        IntrinsicTypeConfig(
+            primitive=True,
+        ),
+    TypeName("String"):
+        IntrinsicTypeConfig(
+            primitive=True,
+        ),
+    TypeName("OpenModelica.$Code.VariableName"):
+        IntrinsicTypeConfig(
+            primitive=False,
+        ),
+    TypeName("OpenModelica.$Code.VariableNames"):
+        IntrinsicTypeConfig(
+            primitive=False,
+        ),
+    TypeName("OpenModelica.$Code.TypeName"):
+        IntrinsicTypeConfig(
+            primitive=False,
+        ),
+}
+
+
+# @AbstractTypeProfile.register_concrete_class
+class IntrinsicTypeProfile(
+    AbstractTypeProfile,
+):
+    @classmethod
+    def match(
+        cls,
+        root: xml._Element,
+        name: TypeName,
+    ) -> bool:
+        return name in __intrinsicTypeConfigs
