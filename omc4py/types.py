11d6f463fe6ef7473fdb8cebf4582a325425ecd6
ijknabla <ijknabla@gmail.com>
2020-10-26 12:28:11 +0900

    Update to Enumeration.__getitem__ accepts typeName

--- omc4py/types.py
+++ omc4py/types.py
@@ -1,14 +1,46 @@
 
 import enum
 
-from .primitive_types import TypeName
+from .primitive_types import (
+    TypeName,
+    VariableName,
+)
+
+
+class EnumerationMeta(
+    enum.EnumMeta,
+):
+    __className__: TypeName
+
+    def __getitem__(
+        cls,
+        key,
+    ):
+        if isinstance(key, VariableName):
+            return super().__getitem__(str(key))
+        elif isinstance(key, TypeName):
+            if not key.parent == cls.__className__:
+                raise KeyError(
+                    "key must be in {className}.{{{enumerator_list}}}"
+                    ", got {key}".format(
+                        className=cls.__className__,
+                        enumerator_list=", ".join(cls.__members__),
+                        key=key,
+                    )
+                )
+            return cls[key.last_identifier]
+        else:
+            try:
+                return cls[VariableName(key)]
+            except ValueError:
+                pass
+            return cls[TypeName(key)]
 
 
 class Enumeration(
     enum.Enum,
+    metaclass=EnumerationMeta,
 ):
-    __className__: TypeName
-
     def __str__(
         self,
     ) -> str:
