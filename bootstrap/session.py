f67a28e11d11d61169e759ba7732c8b054db5a71
ijknabla <ijknabla@gmail.com>
2020-10-28 16:55:41 +0900

    Add OMCSessionBootstrap

---
+++ bootstrap/session.py
@@ -0,0 +1,59 @@
+
+import typing
+
+from omc4py.parser import (
+    parse_OMCValue,
+)
+from omc4py.session import (
+    OMCSessionMinimal,
+    cast_value,
+)
+from omc4py.types import (
+    Boolean,
+    TypeName,
+    VariableName,
+)
+
+
+class OMCSessionBootstrap(
+    OMCSessionMinimal,
+):
+    def getClassNames(
+        self,
+        class_: typing.Optional[TypeName] = None,
+        recursive: typing.Optional[bool] = None,
+        qualified: typing.Optional[bool] = None,
+    ) -> typing.List[TypeName]:
+        # Check arguments
+        class___internal__ = cast_value(
+            "class_", class_,
+            optional=True,
+            class_=TypeName,
+            class_restrictions=(),
+            sizes=(),
+        )
+        recursive__internal__ = cast_value(
+            "recursive", recursive,
+            optional=True,
+            class_=Boolean,
+            class_restrictions=(Boolean,),
+            sizes=(),
+        )
+        qualified__internal__ = cast_value(
+            "qualified", qualified,
+            optional=True,
+            class_=Boolean,
+            class_restrictions=(Boolean,),
+            sizes=(),
+        )
+
+        # Call function
+        return self.__omc_call__(
+            "getClassNames",
+            kwrds={
+                VariableName("class_"): class___internal__,
+                VariableName("recursive"): recursive__internal__,
+                VariableName("qualified"): qualified__internal__,
+            },
+            parser=parse_OMCValue,
+        )
