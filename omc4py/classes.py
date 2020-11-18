3e2a73d1c37901c23c9a3b41c04afea43c634d23
ijknabla <ijknabla@gmail.com>
2020-11-18 15:09:48 +0900

    Remove unused __omc_call__

--- omc4py/classes.py
+++ omc4py/classes.py
@@ -3,11 +3,6 @@ import abc
 import typing
 import typing_extensions
 
-from . import (
-    string,
-    types,
-)
-
 
 Dimensions = typing.Tuple[typing.Optional[int], ...]
 
@@ -114,11 +109,6 @@ class AbstractOMCInteractive(
         return False
 
 
-PositionalArguments = typing.Sequence[typing.Any]
-KeywordArguments = typing.Mapping[types.VariableName, typing.Any]
-Parser = typing.Callable[[str], typing.Any]
-
-
 class AbstractOMCSession(
     abc.ABC,
 ):
@@ -133,47 +123,6 @@ class AbstractOMCSession(
     @property
     def __omc__(self) -> AbstractOMCInteractive:
         return self.__omc
-
-    def __omc_call__(
-        self,
-        funcName: str,
-        *,
-        args: typing.Optional[PositionalArguments] = None,
-        kwrds: typing.Optional[KeywordArguments] = None,
-        parser: typing.Optional[Parser] = None,
-        check: bool = True,
-    ) -> typing.Any:
-        argument_literals: typing.List[str] = []
-        if args is not None:
-            for arg in args:
-                argument_literals.append(
-                    string.to_omc_literal(arg)
-                )
-        if kwrds is not None:
-            for ident, value in kwrds.items():
-                if value is None:
-                    continue
-                value_literal = string.to_omc_literal(
-                    value
-                )
-                argument_literals.append(
-                    f"{ident}={value_literal}"
-                )
-
-        result_literal = self.__omc__.evaluate(
-            "{}({})".format(
-                funcName,
-                ", ".join(argument_literals)
-            )
-        )
-
-        if check:
-            self.__omc_check__()
-
-        if parser is None:
-            return result_literal
-        else:
-            return parser(result_literal)
 
     @abc.abstractmethod
     def __omc_check__(self) -> None: ...
