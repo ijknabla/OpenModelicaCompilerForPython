20a6c640ba73bc79fa95ec39d45be9127b1d5cd1
ijknabla <ijknabla@gmail.com>
2020-11-18 16:11:18 +0900

    Remove OMCEnumeration

--- omc4py/types.py
+++ omc4py/types.py
@@ -1,17 +1,13 @@
 
 __all__ = (
     "Boolean",
-    "Component",
     "Integer",
-    "OMCEnumeration",
     "Real",
     "String",
     "TypeName",
     "VariableName",
 )
 
-
-import enum
 import numpy  # type: ignore
 import typing
 
@@ -206,57 +202,6 @@ class TypeName(
         other: typing.Union[str, VariableName, "TypeName"]
     ):
         return type(self)(self, other)
-
-
-class OMCEnumerationMeta(
-    enum.EnumMeta,
-):
-    __className__: TypeName
-
-    def __call__(
-        cls,
-        value,
-    ):
-        if isinstance(value, VariableName):
-            return cls[str(value)] \
-                # pylint: disable=unsubscriptable-object
-        if isinstance(value, TypeName):
-            if not value.parent == cls.__className__:
-                raise KeyError(
-                    "TypeName must be in {className}.{{{enumerator_list}}}"
-                    ", got {value}".format(
-                        className=cls.__className__,
-                        enumerator_list=", ".join(cls.__members__),
-                        value=value,
-                    )
-                )
-            return cls(value.last_identifier) \
-                # pylint: disable=no-value-for-parameter
-        elif isinstance(value, str):
-            try:
-                return cls(VariableName(value)) \
-                    # pylint: disable=no-value-for-parameter
-            except ValueError:
-                pass
-            return cls(TypeName(value)) \
-                # pylint: disable=no-value-for-parameter
-        else:
-            return super().__call__(value)
-
-
-class OMCEnumeration(
-    Integer,  # type: ignore
-    enum.Enum,
-    metaclass=OMCEnumerationMeta,
-):
-    __className__: typing.ClassVar[TypeName]
-
-    def __str__(
-        self,
-    ) -> str:
-        return f"{self.__className__}.{self.name}"
-
-    __to_omc_literal__ = __str__
 
 
 from . import parser  # noqa: E402
