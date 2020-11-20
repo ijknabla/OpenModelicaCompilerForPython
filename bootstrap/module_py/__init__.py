b113c1334325cf067a65491abf48dcb422d4d1dd
ijknabla <ijknabla@gmail.com>
2020-11-20 16:48:17 +0900

    Implement recursive code generation

--- bootstrap/module_py/__init__.py
+++ bootstrap/module_py/__init__.py
@@ -1,10 +1,116 @@
 
+import abc
+import collections
 from lxml import etree  # type: ignore
+import typing
 
-from .code import Code
+from .code import (
+    AbstractCode,
+    Code,
+    CodeWithIndent,
+    empty_line,
+)
+
+from omc4py.classes import (
+    TypeName
+)
+
+
+class AbstractModelicaClass(
+    abc.ABC,
+):
+    element: etree._Element
+    children: typing.MutableMapping[str, "AbstractModelicaClass"]
+
+    def __init__(
+        self,
+        element: etree._Element
+    ):
+        self.element = element
+        self.children = collections.OrderedDict()
+
+    @property
+    def className(
+        self
+    ) -> TypeName:
+        return TypeName(self.element.attrib["id"])
+
+    def __getitem__(
+        self,
+        index: TypeName
+    ) -> "AbstractModelicaClass":
+        if index == self.className:
+            return self
+
+        cursor = index.parts[len(self.className.parts)]
+        return self.children[cursor][index]
+
+    def add_class(
+        self,
+        class_: "AbstractModelicaClass",
+    ):
+        className = class_.className
+        parent = self[className.parent]
+        parent.children[str(className.last_identifier)] = class_
+
+    def generate_class_codes(
+        self,
+    ) -> typing.Iterator[AbstractCode]:
+        for child_class in self.children.values():
+            yield child_class.to_code()
+
+    @abc.abstractmethod
+    def to_code(
+        self,
+    ) -> AbstractCode:
+        return NotImplemented
+
+
+class GenericModelicaClass(
+    AbstractModelicaClass,
+):
+    def to_code(
+        self
+    ) -> AbstractCode:
+        return Code(
+            f"class {self.className.last_identifier}(",
+            "):",
+            CodeWithIndent(
+                "...",
+                *self.generate_class_codes(),
+                sep=empty_line,
+            ),
+        )
 
 
 def generate_module_py(
     omc_interface_xml: etree._ElementTree,
 ) -> Code:
-    return Code()
+    return Code(
+        empty_line,
+        generate_nested_modelica_class(
+            omc_interface_xml.xpath('//*[@id]')
+        ).to_code(),
+    )
+
+
+def generate_nested_modelica_class(
+    elements: typing.List[etree._Element],
+) -> AbstractModelicaClass:
+    if not elements:
+        raise ValueError(
+            "elements are empty!"
+        )
+    elements_iter = iter(elements)
+
+    root_modelica_class = generate_modelica_class(next(elements_iter))
+    for modelica_class in map(generate_modelica_class, elements_iter):
+        root_modelica_class.add_class(modelica_class)
+
+    return root_modelica_class
+
+
+def generate_modelica_class(
+    element: etree._Element,
+) -> AbstractModelicaClass:
+    return GenericModelicaClass(element)
