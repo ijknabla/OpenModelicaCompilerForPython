addbac2531b007057d077a18232e15fff41ef2e8
ijknabla <ijknabla@gmail.com>
2020-12-03 00:41:25 +0900

    Add OpenModelica package assertion

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,7 +1,23 @@
 
-from omc4py import open_session
+from omc4py import (
+    TypeName,
+    open_session,
+)
+import pytest
 
 
 def test_open_session():
     with open_session() as session:
         assert session is not None
+
+
+@pytest.fixture
+def session():
+    with open_session() as session:
+        yield session
+
+
+def test_OpenModelica(
+    session  # fixture
+):
+    assert session.isPackage("OpenModelica")
