7b00b5956c4626765b7d121439e6f9b8f97dec47
ijknabla <ijknabla@gmail.com>
2020-12-03 00:46:40 +0900

    Add test for loadString

--- tests/test_session.py
+++ tests/test_session.py
@@ -17,6 +17,21 @@ def session():
         yield session
 
 
+def test_loadString(
+    session  # fixture
+):
+    assert session.loadString("type MyEnumeration = enumeration(e, n, u, m);")
+    assert session.isType("MyEnumeration")
+    assert session.isEnumeration("MyEnumeration")
+    assert session.loadString("package MyPackage end MyPackage;")
+    assert session.isPackage("MyPackage")
+    assert session.loadString("record MyRecord end MyRecord;")
+    assert session.isRecord("MyRecord")
+    assert session.loadString("function MyFunction end MyFunction;")
+    assert session.isFunction("MyFunction")
+    session.__check__()
+
+
 def test_OpenModelica(
     session  # fixture
 ):
