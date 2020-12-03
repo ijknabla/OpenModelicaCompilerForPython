07fa05670a99b124f45fd5b96655eabda359e422
ijknabla <ijknabla@gmail.com>
2020-12-03 09:56:57 +0900

    Add test for getClassNames

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,5 +1,6 @@
 
 from omc4py import (
+    TypeName,
     open_session,
 )
 import pytest
@@ -32,6 +33,95 @@ def test_loadString(
     assert session.isRecord("MyRecord")
     assert session.loadString("function MyFunction end MyFunction;")
     assert session.isFunction("MyFunction")
+
+
+@pytest.mark.dependency(depends=["test_open_session"])
+def test_getClassNames(
+    session  # fixture
+):
+    assert session.loadString(
+        """
+package Test_getClassNames
+end Test_getClassNames;
+        """
+    )
+    assert session.isPackage("Test_getClassNames")
+    assert 0 == len(session.getClassNames("Test_getClassNames"))
+
+    assert session.loadString(
+        """
+within Test_getClassNames;
+type C = enumeration(c);
+        """
+    )
+    assert (
+        session.getClassNames("Test_getClassNames") == [
+            TypeName("C"),
+        ]
+    ).all()
+    assert (
+        session.getClassNames("Test_getClassNames", sort=True) == [
+            TypeName("C"),
+        ]
+    ).all()
+    assert (
+        session.getClassNames("Test_getClassNames", qualified=True) == [
+            TypeName("Test_getClassNames.C"),
+        ]
+    ).all()
+
+    assert session.loadString(
+        """
+within Test_getClassNames;
+type A = enumeration(a);
+        """
+    )
+    assert (
+        session.getClassNames("Test_getClassNames") == [
+            TypeName("C"),
+            TypeName("A"),
+        ]
+    ).all()
+    assert (
+        session.getClassNames("Test_getClassNames", sort=True) == [
+            TypeName("A"),
+            TypeName("C"),
+        ]
+    ).all()
+    assert (
+        session.getClassNames("Test_getClassNames", qualified=True) == [
+            TypeName("Test_getClassNames.C"),
+            TypeName("Test_getClassNames.A"),
+        ]
+    ).all()
+
+    assert session.loadString(
+        """
+within Test_getClassNames;
+type B = enumeration(b);
+        """
+    )
+    assert (
+        session.getClassNames("Test_getClassNames") == [
+            TypeName("C"),
+            TypeName("A"),
+            TypeName("B"),
+        ]
+    ).all()
+    assert (
+        session.getClassNames("Test_getClassNames", sort=True) == [
+            TypeName("A"),
+            TypeName("B"),
+            TypeName("C"),
+        ]
+    ).all()
+    assert (
+        session.getClassNames("Test_getClassNames", qualified=True) == [
+            TypeName("Test_getClassNames.C"),
+            TypeName("Test_getClassNames.A"),
+            TypeName("Test_getClassNames.B"),
+        ]
+    ).all()
 
 
 @pytest.mark.dependency(depends=["test_open_session"])
