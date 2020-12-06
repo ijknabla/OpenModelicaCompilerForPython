8943307ab36fceba7b592107353ea3000b97d6c3
ijknabla <ijknabla@gmail.com>
2020-12-06 13:40:46 +0900

    Add test call

--- .github/actions/pytest/entrypoint.sh
+++ .github/actions/pytest/entrypoint.sh
@@ -6,3 +6,5 @@ omc --version
 
 useradd -m user
 sudo -u user python -m pip install ${GITHUB_WORKSPACE}
+sudo -u user python -m pip install -r ${GITHUB_WORKSPACE}/tests/requirements.txt
+sudo -u user /home/user/.local/bin/pytest -v ${GITHUB_WORKSPACE}
