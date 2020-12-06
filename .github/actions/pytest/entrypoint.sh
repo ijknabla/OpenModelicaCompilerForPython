76fb8e8f4719f9a7601a7ff18fe84ac48cc6f743
ijknabla <ijknabla@gmail.com>
2020-12-06 13:42:12 +0900

    Add $USER Environ

--- .github/actions/pytest/entrypoint.sh
+++ .github/actions/pytest/entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 python --version
 omc --version
 
-useradd -m user
-sudo -u user python -m pip install ${GITHUB_WORKSPACE}
-sudo -u user python -m pip install -r ${GITHUB_WORKSPACE}/tests/requirements.txt
-sudo -u user /home/user/.local/bin/pytest -v ${GITHUB_WORKSPACE}
+useradd -m ${USER}
+sudo -u ${USER} python -m pip install ${GITHUB_WORKSPACE}
+sudo -u ${USER} python -m pip install -r ${GITHUB_WORKSPACE}/tests/requirements.txt
+sudo -u ${USER} /home/${USER}/.local/bin/pytest -v ${GITHUB_WORKSPACE}
