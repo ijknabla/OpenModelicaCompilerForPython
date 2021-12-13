4bbfe8f768d54248c71fe32a714901f093025436
ijknabla <ijknabla@gmail.com>
2021-12-13 11:45:25 +0900

    Try to use poetry environment

--- .github/actions/pytest-omc1.13.0/entrypoint.sh
+++ .github/actions/pytest-omc1.13.0/entrypoint.sh
@@ -5,6 +5,7 @@ python --version
 omc --version
 
 useradd -m ${USER}
-sudo -u ${USER} python -m pip install ${GITHUB_WORKSPACE}
-sudo -u ${USER} python -m pip install -r ${GITHUB_WORKSPACE}/tests/requirements.txt
-sudo -u ${USER} /home/${USER}/.local/bin/pytest -v ${GITHUB_WORKSPACE}
+sudo -u ${USER} python -m pip install poetry
+sudo -u ${USER} cd ${GITHUB_WORKSPACE}
+sudo -u ${USER} python -m poetry install
+sudo -u ${USER} python -m poetry run pytest
