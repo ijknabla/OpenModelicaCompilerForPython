f05e27f6cc856adb2d49792cc3ded94fa0213098
ijknabla <ijknabla@gmail.com>
2021-12-13 12:03:41 +0900

    Move cd command

--- .github/actions/pytest-omc1.13.0/entrypoint.sh
+++ .github/actions/pytest-omc1.13.0/entrypoint.sh
@@ -4,8 +4,9 @@ set -eu
 python --version
 omc --version
 
+cd ${GITHUB_WORKSPACE}
+
 useradd -m ${USER}
 sudo -u ${USER} python -m pip install poetry
-sudo -u ${USER} cd ${GITHUB_WORKSPACE}
 sudo -u ${USER} python -m poetry install
 sudo -u ${USER} python -m poetry run pytest
