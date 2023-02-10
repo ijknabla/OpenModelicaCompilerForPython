8e4f06ba019d345006899dd3505188b011e7d81b
ijknabla <ijknabla@gmail.com>
2023-02-10 23:50:56 +0900

    Add test for absolute typename

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -15,3 +15,14 @@ def test_typename_parts() -> None:
 
     for i, parent in enumerate(typename.parents, start=1):
         assert parent.parts == typename.parts[:-i]
+
+
+def test_absolute_typename() -> None:
+    typename = TypeName("A.B.C.D")
+    assert not typename.is_absolute
+    assert typename.as_absolute().is_absolute
+
+    assert f"{typename.as_absolute()}" == f".{typename}"
+    assert typename.as_absolute().parts == (".", *typename.parts)
+
+    assert typename.as_absolute().as_absolute() == typename.as_absolute()
