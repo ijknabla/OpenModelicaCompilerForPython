4454fb32f20ff6eaf1578cbd8324d1edfb684b1d
ijknabla <ijknabla@gmail.com>
2023-02-10 23:44:08 +0900

    Add test for TypeName.parents

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -6,3 +6,10 @@ def test_typename_combine() -> None:
     parts_b = ("B1", "B2")
 
     assert (TypeName(*parts_a) / TypeName(*parts_b)).parts == parts_a + parts_b
+
+
+def test_typename_parents() -> None:
+    typename = TypeName("A.B.C.D")
+
+    for i, parent in enumerate(typename.parents, start=1):
+        assert parent.parts == typename.parts[:-i]
