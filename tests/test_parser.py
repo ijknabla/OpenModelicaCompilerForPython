a88971d79188e528e509b6a93875e0be270634b7
ijknabla <ijknabla@gmail.com>
2023-02-14 14:21:50 +0900

    Separate expected value

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,18 @@
+from typing import Any
+
 import pytest
 
 from omc4py.parser import parse_OMCValue
 
 
-@pytest.mark.parametrize("literal", ["+1.0", "-1.0", "+1", "-1"])
-def test_parse_primitives(literal: str) -> None:
-    assert eval(literal) == parse_OMCValue(literal)
+@pytest.mark.parametrize(
+    "literal, expected",
+    [
+        ("+1.0", +1.0),
+        ("-1.0", -1.0),
+        ("+1", +1),
+        ("-1", -1),
+    ],
+)
+def test_parse_primitives(literal: str, expected: Any) -> None:
+    assert parse_OMCValue(literal) == expected
