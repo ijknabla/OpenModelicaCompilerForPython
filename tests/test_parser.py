18a12f500567f5bc51ad42aa7a1c28f14e1e3c89
ijknabla <ijknabla@gmail.com>
2023-02-14 14:32:14 +0900

    Add test for sequence

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import pytest
+from numpy import array
 
 from omc4py.classes import TypeName
 from omc4py.parser import parse_OMCValue
@@ -17,7 +18,21 @@ from omc4py.parser import parse_OMCValue
         ("false", False),
         ('"text"', "text"),
         ("A.$a", TypeName("A.$a")),
+        ("()", ()),
+        ("(0, 1)", (0, 1)),
     ],
 )
 def test_parse_primitives(literal: str, expected: Any) -> None:
     assert parse_OMCValue(literal) == expected
+
+
+@pytest.mark.parametrize(
+    "literal, expected",
+    [
+        ("{}", []),
+        ("{0, 1}", [0, 1]),
+        ("{{0, 1}, {2, 3}}", [[0, 1], [2, 3]]),
+    ],
+)
+def test_parse_arrays(literal: str, expected: Any) -> None:
+    assert (parse_OMCValue(literal) == array(expected)).all()
