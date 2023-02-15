a8f267c145422c78b074bb18d050c26e9e00145e
ijknabla <ijknabla@gmail.com>
2023-02-15 14:06:54 +0900

    Add imports to array-stub

--- bootstrap/array.py
+++ bootstrap/array.py
@@ -4,6 +4,7 @@ from typing import IO
 
 import click
 
+from .ast_generator import iter_import_froms
 from .backport import unparse
 
 
@@ -11,7 +12,16 @@ from .backport import unparse
 @click.option("-o", "--output", type=click.File("w"), default=sys.stdout)
 def main(output: IO[str]) -> None:
     module = Module(
-        body=[],
+        body=[
+            *iter_import_froms(
+                [
+                    ("__future__", ["annotations"]),
+                    ("collections.abc", ["Sequence"]),
+                    ("typing", ["Generic", "TypeVar", "Union", "overload"]),
+                    ("typing_extensions", ["Literal"]),
+                ]
+            ),
+        ],
         type_ignores=[],
     )
     output.write(unparse(module))
