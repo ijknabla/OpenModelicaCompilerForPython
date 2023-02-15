dd366ae107a5748a3d2b731f89e33d22f1fc5dce
ijknabla <ijknabla@gmail.com>
2023-02-15 14:10:43 +0900

    Add --max-dim & --max-size to bootstrap.array

--- bootstrap/array.py
+++ bootstrap/array.py
@@ -6,11 +6,16 @@ import click
 
 from .ast_generator import iter_import_froms
 from .backport import unparse
+from .util import countup
 
 
 @click.command
 @click.option("-o", "--output", type=click.File("w"), default=sys.stdout)
-def main(output: IO[str]) -> None:
+@click.option("--max-dim", type=int, required=True)
+@click.option("--max-size", type=int, required=True)
+def main(output: IO[str], max_dim: int, max_size: int) -> None:
+    countup(1, max_dim)
+    countup(1, max_size)
     module = Module(
         body=[
             *iter_import_froms(
