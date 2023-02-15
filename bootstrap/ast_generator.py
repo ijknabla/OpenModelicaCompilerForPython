6a6cf20399e2164bfb990b48bd7257ce0e606d23
ijknabla <ijknabla@gmail.com>
2023-02-15 14:40:13 +0900

    Implement array_overload_function_defs

--- bootstrap/ast_generator.py
+++ bootstrap/ast_generator.py
@@ -12,6 +12,7 @@ from ast import (
     Index,
     Load,
     Name,
+    NameConstant,
     Num,
     Store,
     Str,
@@ -24,6 +25,7 @@ from ast import (
     keyword,
 )
 from collections.abc import Iterable, Iterator
+from itertools import product
 from typing import NewType
 
 from .util import countup
@@ -306,3 +308,101 @@ def _ndarray_class_def(dim: Dimension) -> ClassDef:
         ],
         decorator_list=[],
     )
+
+
+def iter_array_overload_function_defs(
+    dims: Iterable[int],
+) -> Iterator[FunctionDef]:
+    for dim in map(Dimension, [0, *dims]):
+        yield from array_overload_function_defs(dim)
+
+
+def array_overload_function_defs(dim: Dimension) -> Iterator[FunctionDef]:
+    annotation = Name(id="DType", ctx=Load())
+    for _ in range(dim):
+        annotation = Subscript(
+            value=Name(id="Sequence", ctx=Load()),
+            slice=Index(value=annotation),
+            ctx=Load(),
+        )
+
+    object_arg = arg(arg="object", annotation=annotation)
+    dtype_kwonlyarg = arg(
+        arg="dtype",
+        annotation=Subscript(
+            value=Name(id="Optional", ctx=Load()),
+            slice=Index(
+                value=Subscript(
+                    value=Name(id="type", ctx=Load()),
+                    slice=Index(value=Name(id="DType", ctx=Load())),
+                    ctx=Load(),
+                )
+            ),
+            ctx=Load(),
+        ),
+    )
+
+    for explicit_sequence in product(*([(True, False)] * dim)):
+        shape_kwonlyarg = arg(
+            arg="shape",
+            annotation=Subscript(
+                value=Name(id="tuple", ctx=Load()),
+                slice=Index(
+                    value=Tuple(
+                        elts=[
+                            Name(id=f"SizeArg{i}", ctx=Load())
+                            if explicit
+                            else NameConstant(value=None)
+                            for i, explicit in enumerate(
+                                explicit_sequence, start=1
+                            )
+                        ],
+                        ctx=Load(),
+                    )
+                ),
+                ctx=Load(),
+            ),
+        )
+
+        if dim == 0:
+            returns = Name(id="DType", ctx=Load())
+        else:
+            returns = Subscript(
+                value=Name(id=f"_NDArray__{dim}", ctx=Load()),
+                slice=Index(
+                    value=Tuple(
+                        elts=[
+                            Name(id="DType", ctx=Load()),
+                            *(
+                                Name(
+                                    id=f"SizeArg{i}" if explicit else "int",
+                                    ctx=Load(),
+                                )
+                                for i, explicit in enumerate(
+                                    explicit_sequence, start=1
+                                )
+                            ),
+                        ],
+                        ctx=Load(),
+                    )
+                ),
+                ctx=Load(),
+            )
+
+        yield FunctionDef(
+            name="array",
+            args=arguments(
+                args=[object_arg],
+                vararg=None,
+                kwonlyargs=[
+                    dtype_kwonlyarg,
+                    shape_kwonlyarg,
+                ],
+                kw_defaults=[NameConstant(value=None), None],
+                kwarg=None,
+                defaults=[],
+            ),
+            body=[Expr(value=Ellipsis())],
+            decorator_list=[Name(id="overload", ctx=Load())],
+            returns=returns,
+        )
