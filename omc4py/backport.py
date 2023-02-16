f3dbacd9c7f49537f743ddcf11b343a6a0ec7828
ijknabla <ijknabla@gmail.com>
2023-02-16 14:36:24 +0900

    Add backport package

---
+++ omc4py/backport.py
@@ -0,0 +1,17 @@
+__all__ = ("Sequence",)
+
+import sys
+from typing import TYPE_CHECKING, Generic, TypeVar
+
+T = TypeVar("T")
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+else:
+    if sys.version_info >= (3, 9):
+        from collections.abc import Sequence
+    else:
+        from collections.abc import Sequence as _Sequence
+
+        class Sequence(_Sequence, Generic[T]):
+            ...
