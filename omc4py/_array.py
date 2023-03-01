1a42a2a25f5cd73fc51c913d1c0ed1876feed5a1
ijknabla <ijknabla@gmail.com>
2023-03-01 17:40:56 +0900

    Update array implementation

--- omc4py/_array.py
+++ omc4py/_array.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
 import builtins
-from collections.abc import Iterable, Iterator, Sequence
+from collections.abc import Iterator, Sequence
 from dataclasses import InitVar, dataclass, field
-from functools import reduce
-from itertools import product
-from operator import getitem
-from typing import Any, ClassVar, Optional, TypeVar, Union, cast, overload
+from functools import partial, reduce
+from itertools import chain, islice, product
+from typing import Any, ClassVar, Optional, TypeVar, Union, cast
 from weakref import WeakValueDictionary
 
-from typing_extensions import Literal, TypeAlias, TypedDict
+from typing_extensions import TypeAlias, TypedDict
 
 from .meta import SupportsArrayIndexing
 
@@ -22,7 +21,7 @@ Shape: TypeAlias = "tuple[Size, ...]"
 
 class _ArrayMetaNameSpace(TypedDict):
     dtype: DType
-    __shape__: Shape
+    shape: Shape
 
 
 class _ArrayMeta(type):
@@ -33,7 +32,7 @@ class _ArrayMeta(type):
     ]
     __cache = WeakValueDictionary()
     dtype: DType = object
-    __shape__: Shape = ()
+    shape: Shape = ()
 
     def __repr__(cls) -> str:
         if cls.__is_root:
@@ -41,7 +40,7 @@ class _ArrayMeta(type):
         else:
             return (
                 f"{_format_dtype(cls)}"
-                f"[{_format_dtype(cls.dtype)}, {cls.__shape__!r}]"
+                f"[{_format_dtype(cls.dtype)}, {cls.shape!r}]"
             )
 
     def __getitem__(
@@ -53,8 +52,8 @@ class _ArrayMeta(type):
                 f"There are no dtype, shape variables left in {cls!r}"
             )
 
-        dtype, __shape__ = _sanitize_array_type_index(index)
-        key = (cls, frozenset(_iter_dtype(dtype)), __shape__)
+        dtype, shape = _sanitize_array_type_index(index)
+        key = (cls, frozenset(_iter_dtype(dtype)), shape)
 
         try:
             return cast(T_array_meta, cls.__cache[key])
@@ -64,7 +63,7 @@ class _ArrayMeta(type):
         indexed_cls = type(cls)(
             cls.__name__,
             (cls,),
-            dict(_ArrayMetaNameSpace(dtype=dtype, __shape__=__shape__)),
+            dict(_ArrayMetaNameSpace(dtype=dtype, shape=shape)),
         )
         cls.__cache[key] = indexed_cls
 
@@ -72,125 +71,149 @@ class _ArrayMeta(type):
 
     @property
     def __is_root(cls) -> bool:
-        return cls.dtype is object and cls.__shape__ == ()
+        return cls.dtype is object and cls.shape == ()
 
 
 @dataclass(frozen=True)
 class Array(metaclass=_ArrayMeta):
     dtype: ClassVar[DType]
-    __shape__: ClassVar[Shape]
     object: InitVar[Any]
     shape: tuple[int, ...] = field(init=False)
-    __data__: list[Any] = field(init=False)
+    ndim: int = field(init=False)
+    __array__: tuple[Any, ...] = field(init=False)
 
     def __post_init__(self, object: Any) -> None:
         shape = self.__assume_shape(object)
         self.__check_shape(object, *shape)
-        self.__setattr("shape", shape)
 
-        __data__ = self.__make_empty(*shape)
-        for indices, value in self.__ndenumerate(object, *shape):
+        array = tuple(
+            reduce(lambda x, _: chain.from_iterable(x), shape[:-1], object)
+        )
+        for i, value in enumerate(array):
             if not isinstance(value, self.dtype):
                 raise TypeError(
                     "object"
-                    + "".join(f"[{i}]" for i in indices)
+                    + "".join(f"[{i}]" for i in Array.__unpack(shape, i))
                     + f" must be instance of {self.dtype}, "
                     f"got {value}: {type(value)}"
                 )
-            self.__set_by_indices(__data__, *indices, value=value)
+        self.__setup(shape, array)
 
-        self.__setattr("__data__", __data__)
+    def __setup(self, shape: tuple[int, ...], array: tuple[Any, ...]) -> None:
+        object.__setattr__(self, "shape", shape)
+        object.__setattr__(self, "ndim", len(shape))
+        object.__setattr__(self, "__array__", array)
 
     @property
-    def ndim(self) -> int:
-        return len(self.__shape__)
+    def __nested(self) -> list[Any]:
+        def impl(iterator: Iterator[Any], size: int, *sizes: int) -> list[Any]:
+            if not sizes:
+                return list(islice(iterator, size))
+            else:
+                return [impl(iterator, *sizes) for _ in range(size)]
+
+        return impl(iter(self.__array__), *self.shape)
+
+    @property
+    def __shape(self) -> Shape:
+        return cast(_ArrayMeta, type(self)).shape
 
     def __len__(self) -> int:
         return self.shape[0]
 
     def __eq__(self, other: Any) -> bool:
         if isinstance(other, Array):
-            return bool(self.__data__ == other.__data__)
+            return (
+                self.shape == other.shape and self.__array__ == other.__array__
+            )
         else:
-            return bool(self.__data__ == other)
+            return bool(self.__nested == other)
 
     def __repr__(self) -> str:
-        return f"{type(self)!r}({self.__data__})"
+        return f"{type(self)!r}({self.__nested})"
 
     def __getitem__(
         self, index: Union[int, slice, tuple[Union[int, slice]]]
     ) -> Any:
+        sanitized = self.__sanitize_index(index)
+
+        shape: tuple[int, ...] = ()
+        indices: list[Sequence[int]] = []
+        for i, size in zip(sanitized, self.shape):
+            if isinstance(i, int):
+                if i not in range(-size, size):
+                    raise IndexError(
+                        f"{type(self).__name__} index out of range"
+                    )
+                indices.append((range(size)[i],))
+            else:
+                r = range(size)[i]
+                indices.append(r)
+                shape += (len(r),)
+
+        if not shape:
+            return self.__array__[
+                self.__pack(self.shape, next(product(*indices)))
+            ]
+
+        __array__ = tuple(  # noqa: F841
+            map(
+                self.__array__.__getitem__,
+                map(partial(self.__pack, self.shape), product(*indices)),
+            )
+        )
+
+        sliced_type: type[Array]
+        sliced_type = Array[self.dtype, shape]
+        sliced = sliced_type.__new__(sliced_type)
+        sliced.__setup(
+            shape,
+            tuple(
+                map(
+                    self.__array__.__getitem__,
+                    map(partial(self.__pack, self.shape), product(*indices)),
+                )
+            ),
+        )
+
+        return sliced
+
+    @staticmethod
+    def __pack(shape: tuple[int, ...], indices: tuple[int, ...]) -> int:
+        result = 0
+        for s, i in zip(shape, indices):
+            result *= s
+            result += i
+        return result
+
+    @staticmethod
+    def __unpack(shape: tuple[int, ...], index: int) -> tuple[int, ...]:
+        indices: list[int] = []
+        for size in reversed(shape):
+            indices.append(index % size)
+            index //= size
+        return tuple(reversed(indices))
+
+    def __sanitize_index(
+        self, index: Union[int, slice, tuple[Union[int, slice], ...]]
+    ) -> tuple[Union[int, slice], ...]:
         indices: tuple[Union[int, slice], ...]
-        if isinstance(index, (int, slice)):
-            indices = (index,)
-        elif isinstance(index, tuple) and all(
-            isinstance(i, (int, slice)) for i in index
-        ):
+        if isinstance(index, tuple):
             indices = index
         else:
-            raise TypeError(
-                f"index must be int | slice | tuple[int | slice], got {index}"
-            )
+            indices = (index,)
 
-        if not (len(indices) <= self.ndim):
+        if len(indices) > self.ndim:
             raise IndexError(
                 f"index length exceed ndim={self.ndim}, got {len(indices)}"
             )
 
-        for size, index in zip(self.shape, indices):
-            if size == 0 and isinstance(index, int):
-                raise IndexError(f"{type(self).__name__} index out of range")
-
-        def bounded_indices() -> Iterator[Union[int, range]]:
-            for s, i in zip(self.shape, indices):
-                yield ((i if isinstance(i, int) else range(s)[i]))
-            yield from map(range, self.shape[len(indices) :])
-
-        return self.__get_by_bounded_indices(tuple(bounded_indices()))
-
-    def __get_by_bounded_indices(
-        self, indices: Sequence[Union[int, range]]
-    ) -> Any:
-        shape = tuple(
-            len(index) for index in indices if isinstance(index, range)
-        )
-
-        if not shape:
-            return self.__get_by_indices(
-                self.__data__, cast("Sequence[int]", indices)
-            )
-
-        array_type: type[Array]
-        array_type = Array[self.dtype, shape]
-        array = array_type.__new__(array_type)
-        array.__setattr("shape", shape)
-
-        __data__: Any
-        if any(size == 0 for size in shape):
-            __data__ = ...
-        else:
-            __data__ = self.__make_empty(*shape)
-
-            srcs = list(
-                product(*((i,) if isinstance(i, int) else i for i in indices))
-            )
-            for src, (dst, _) in zip(
-                srcs, self.__ndenumerate(__data__, *shape)
-            ):
-                self.__set_by_indices(
-                    __data__,
-                    *dst,
-                    value=self.__get_by_indices(self.__data__, src),
-                )
-
-        array.__setattr("__data__", __data__)
-
-        return array
+        return indices + (slice(None, None),) * (self.ndim - len(indices))
 
     def __assume_shape(self, object: Any) -> tuple[int, ...]:
         result: list[int]
         result = []
-        for i, expected_size in enumerate(self.__shape__):
+        for i, expected_size in enumerate(self.__shape):
             if not isinstance(object, SupportsArrayIndexing):
                 raise TypeError(
                     "object"
@@ -226,58 +249,12 @@ class Array(metaclass=_ArrayMeta):
         for i in range(size):
             cls.__check_shape(object[i], *sizes, indices=(*indices, i))
 
-    @classmethod
-    def __make_empty(cls, size: int, *sizes: int) -> list[Any]:
-        return [
-            cls.__make_empty(*sizes) if sizes else None for _ in range(size)
-        ]
-
-    @classmethod
-    def __ndenumerate(
-        cls, object: Any, size: int, *sizes: int, indices: tuple[int, ...] = ()
-    ) -> Iterator[tuple[tuple[int, ...], Any]]:
-        for i in range(size):
-            if not sizes:
-                yield (*indices, i), object[i]
-            else:
-                yield from cls.__ndenumerate(
-                    object[i], *sizes, indices=(*indices, i)
-                )
-
-    @overload
-    def __setattr(
-        self, __name: Literal["shape"], __value: tuple[int, ...]
-    ) -> None:
-        ...
-
-    @overload
-    def __setattr(
-        self, __name: Literal["__data__"], __value: list[Any]
-    ) -> None:
-        ...
-
-    def __setattr(self, __name: str, __value: Any) -> None:
-        object.__setattr__(self, __name, __value)
-
-    @classmethod
-    def __get_by_indices(cls, object: Any, indices: Iterable[int]) -> Any:
-        return reduce(getitem, indices, object)
-
-    @classmethod
-    def __set_by_indices(
-        cls, object: Any, index: int, *indices: int, value: Any
-    ) -> None:
-        if not indices:
-            object[index] = value
-        else:
-            cls.__set_by_indices(object[index], *indices, value=value)
-
 
 def _sanitize_array_type_index(index: Any) -> tuple[DType, Shape]:
     if not (isinstance(index, tuple) and len(index) == 2):
         raise TypeError(f"index must be a tuple of length 2, got {index}")
 
-    dtype, __shape__ = index
+    dtype, shape = index
     if not (
         isinstance(dtype, type)
         or isinstance(dtype, tuple)
@@ -300,20 +277,20 @@ def _sanitize_array_type_index(index: Any) -> tuple[DType, Shape]:
             )
 
     if not (
-        isinstance(__shape__, tuple)
-        and __shape__
-        and all(isinstance(size, int) or size is None for size in __shape__)
+        isinstance(shape, tuple)
+        and shape
+        and all(isinstance(size, int) or size is None for size in shape)
     ):
         raise TypeError(
             "shape := index[1] must be not empty tuple[int | None, ...], "
-            f"got {__shape__}"
+            f"got {shape}"
         )
 
     t, *ts = sorted(set(_iter_dtype(dtype)), key=_type_order)
     if not ts:
-        return t, __shape__
+        return t, shape
     else:
-        return (t, *ts), __shape__
+        return (t, *ts), shape
 
 
 def _iter_dtype(dtype: DType) -> Iterator[type]:
