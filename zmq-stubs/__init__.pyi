933af5a5f7da8819dc48071d6798ec9b83d60b8a
ijknabla <ijknabla@gmail.com>
2023-06-05 16:37:22 +0900

    Add type-hints for constants

---
+++ zmq-stubs/__init__.pyi
@@ -0,0 +1,11 @@
+from typing import Literal as _Literal
+
+PAIR: _Literal[0]
+PUB: _Literal[1]
+SUB: _Literal[2]
+REQ: _Literal[3]
+REP: _Literal[4]
+DEALER: _Literal[5]
+ROUTER: _Literal[6]
+PULL: _Literal[7]
+PUSH: _Literal[8]
