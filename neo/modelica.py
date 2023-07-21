ef00fa1bde2ba1dc38e111e3a21b2702fa4ac89f
ijknabla <ijknabla@gmail.com>
2023-07-21 14:05:37 +0900

    Implement `external` decorator factory

--- neo/modelica.py
+++ neo/modelica.py
@@ -1,7 +1,18 @@
+from __future__ import annotations
+
 import enum
-from typing import ClassVar
+from collections.abc import Callable
+from functools import partial
+from typing import ClassVar, TypeVar
 
 from .openmodelica import TypeName
+
+_T = TypeVar("_T")
+_T_class = TypeVar("_T_class", "enumeration", "record")
+
+
+def external(class_name: str) -> Callable[[_T], _T]:
+    return partial(_external, class_name=class_name)  # type: ignore
 
 
 class enumeration(enum.Enum):
@@ -10,3 +21,8 @@ class enumeration(enum.Enum):
 
 class record:
     __omc_class__: ClassVar[TypeName]
+
+
+def _external(typ: type[_T_class], class_name: str) -> type[_T_class]:
+    typ.__omc_class__ = TypeName(class_name)
+    return typ
