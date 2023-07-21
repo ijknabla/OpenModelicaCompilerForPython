6c33daaade7e0b0f1379fb3df8436cf0e521fb37
ijknabla <ijknabla@gmail.com>
2023-07-21 14:33:30 +0900

    Add `alias` type-hint

--- neo/modelica.py
+++ neo/modelica.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import enum
 from collections.abc import Callable
 from functools import partial
-from typing import ClassVar, TypeVar
+from typing import ClassVar, Generic, TypeVar
 
 from .openmodelica import TypeName
 
@@ -13,6 +13,10 @@ _T_class = TypeVar("_T_class", "enumeration", "record")
 
 def external(class_name: str) -> Callable[[_T], _T]:
     return partial(_external, class_name=class_name)  # type: ignore
+
+
+class alias(Generic[_T]):
+    ...
 
 
 class enumeration(enum.Enum):
