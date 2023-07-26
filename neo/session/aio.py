0eb83d22534033965110cbed9fecafc89ba8ccd3
ijknabla <ijknabla@gmail.com>
2023-07-26 14:10:47 +0900

    Implement neo\.session(\.aio)?\.Session

---
+++ neo/session/aio.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from contextlib import closing
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Awaitable
+
+from ..protocol import SupportsInteractive
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+
+@dataclass
+class Session:
+    __omc_interactive__: SupportsInteractive[Awaitable[str]]
+
+    def close(self) -> None:
+        self.__omc_interactive__.close()
+
+    def __enter__(self) -> Self:
+        return closing(self).__enter__()
+
+    def __exit__(self, *exc_info: Any) -> None:
+        return closing(self).__exit__()
