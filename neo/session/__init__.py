ce8c04894a6eb9e5a25169019a95ac43d7f95dfa
ijknabla <ijknabla@gmail.com>
2023-07-26 14:43:24 +0900

    Implement getComponents

--- neo/session/__init__.py
+++ neo/session/__init__.py
@@ -2,8 +2,12 @@ from __future__ import annotations
 
 from contextlib import closing
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, List, Union
 
+from omc4py.string import to_omc_literal
+
+from ..openmodelica import Component, TypeName
+from ..parser import cast, parse
 from ..protocol import SupportsInteractive
 
 if TYPE_CHECKING:
@@ -22,3 +26,8 @@ class Session:
 
     def __exit__(self, *exc_info: Any) -> None:
         return closing(self).__exit__()
+
+    def getComponents(self, name: Union[TypeName, str]) -> List[Component]:
+        expression = f"getComponents({to_omc_literal(cast(TypeName, name))})"
+        literal = self.__omc_interactive__.evaluate(expression)
+        return parse(List[Component], literal)
