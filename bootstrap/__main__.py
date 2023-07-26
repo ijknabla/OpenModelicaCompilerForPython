ff9eb86cd8f06e47530586fdc0dacb3ed4daed7d
ijknabla <ijknabla@gmail.com>
2023-07-26 18:03:52 +0900

    Add interface-docker subcommand to bootstrap cli

--- bootstrap/__main__.py
+++ bootstrap/__main__.py
@@ -2,15 +2,16 @@ from __future__ import annotations
 
 import sys
 from asyncio import run
-from collections.abc import Callable, Coroutine
+from collections.abc import Callable, Coroutine, Sequence
 from functools import wraps
+from pathlib import Path
 from typing import IO, Any, TypeVar
 
 import click
 import yaml
 from typing_extensions import ParamSpec
 
-from .interface import create_interface
+from .interface import create_interface, create_interface_by_docker
 
 _P = ParamSpec("_P")
 _T = TypeVar("_T")
@@ -45,6 +46,44 @@ async def interface(n: int, o: IO[str]) -> None:
         await create_interface(n),
         o,
         sort_keys=False,
+    )
+
+
+@main.command()
+@click.option("-i", "--image", type=str, required=True)
+@click.option("-n", type=int, default=1)
+@click.option(
+    "-o",
+    "--output-dir",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, path_type=Path
+    ),
+    default=Path("."),
+)
+@click.option(
+    "--pip-cache-dir",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, path_type=Path
+    ),
+)
+@click.argument(
+    "tag",
+    type=str,
+    nargs=-1,
+)
+@run_decorator
+async def interface_docker(
+    image: str,
+    tag: Sequence[str],
+    n: int,
+    output_dir: Path,
+    pip_cache_dir: Path | None,
+) -> None:
+    await create_interface_by_docker(
+        image=[f"{image}:{t}" for t in tag],
+        n=n,
+        output_dir=output_dir,
+        pip_cache_dir=pip_cache_dir,
     )
 
 
