8a4901820218b419997a1a863c0fd5c132b323c6
ijknabla <ijknabla@gmail.com>
2023-07-28 11:52:25 +0900

    Change fixture scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import pytest
 from neo import latest, open_session
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def session() -> Generator[latest.Session, None, None]:
     with open_session() as session:
         yield session
