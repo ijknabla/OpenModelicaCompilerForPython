301b31b994324e6f31aa212aae54a6ca144582e5
ijknabla <ijknabla@gmail.com>
2023-07-28 15:51:10 +0900

    Implement session type selecting

--- neo/__init__.py
+++ neo/__init__.py
@@ -1,18 +1,66 @@
 from __future__ import annotations
 
-__all__ = ("TypeName", "VariableName")
+__all__ = (
+    "TypeName",
+    "VariableName",
+)
+import re
 from os import PathLike
 from typing import TYPE_CHECKING, Any, Union, overload
 
 from typing_extensions import Literal
 
+import omc4py.exception
+
+from . import session
 from .interactive import open_interactives
 from .openmodelica import TypeName, VariableName
+from .protocol import SupportsInteractive
 
+Command = Union[str, "PathLike[str]"]
 if TYPE_CHECKING:
     from . import latest
 
-Command = Union[str, "PathLike[str]"]
+
+def _select_session_type(
+    version: tuple[int, int]
+) -> tuple[type[session.Session], type[session.aio.Session]]:
+    if version <= (1, 13):
+        from . import v_1_13
+
+        return v_1_13.Session, v_1_13.aio.Session
+    elif version <= (1, 14):
+        from . import v_1_14
+
+        return v_1_14.Session, v_1_14.aio.Session
+    elif version <= (1, 16):
+        from . import v_1_16
+
+        return v_1_16.Session, v_1_16.aio.Session
+    elif version <= (1, 17):
+        from . import v_1_17
+
+        return v_1_17.Session, v_1_17.aio.Session
+    elif version <= (1, 18):
+        from . import v_1_18
+
+        return v_1_18.Session, v_1_18.aio.Session
+    elif version <= (1, 19):
+        from . import v_1_19
+
+        return v_1_19.Session, v_1_19.aio.Session
+    elif version <= (1, 20):
+        from . import v_1_20
+
+        return v_1_20.Session, v_1_20.aio.Session
+    elif version <= (1, 21):
+        from . import v_1_21
+
+        return v_1_21.Session, v_1_21.aio.Session
+    else:
+        from . import latest
+
+        return latest.Session, latest.aio.Session
 
 
 @overload
@@ -42,9 +90,26 @@ def open_session(
         "omc" if omc_command is None else omc_command
     )
 
-    from . import latest
+    try:
+        session_type, aio_session_type = _select_session_type(
+            _get_version(interactive)
+        )
+    except Exception:
+        interactive.close()
+        aio_interactive.close()
+        raise
 
     if asyncio:
-        return latest.aio.Session(aio_interactive)
+        return aio_session_type(aio_interactive)
     else:
-        return latest.Session(interactive)
+        return session_type(interactive)
+
+
+def _get_version(interactive: SupportsInteractive[str]) -> tuple[int, int]:
+    version = interactive.evaluate("getVersion()")
+    matched = re.search(r"(\d+)\.(\d+)", version)
+    if matched is None:
+        raise omc4py.exception.OMCRuntimeError(
+            f"Invalid version string {version!r}"
+        )
+    return tuple(map(int, matched.groups()))  # type: ignore
