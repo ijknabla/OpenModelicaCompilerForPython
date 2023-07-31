b10fdf4a0e528af1022050a525809184f3929cf9
ijknabla <ijknabla@gmail.com>
2023-07-31 20:45:55 +0900

    Update theme

--- document/conf.py
+++ document/conf.py
@@ -13,7 +13,10 @@ author = "ijknabla <ijknabla@gmail.com>"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -22,5 +25,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
