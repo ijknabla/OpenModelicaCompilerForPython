5e74e1e66fd7251ff2be6f64294184f39831a019
ijknabla <ijknabla@gmail.com>
2023-10-30 14:27:42 +0900

    Update by _monkey_patch_typing

---
+++ bootstrap/__init__.py
@@ -0,0 +1,25 @@
+import sys
+import typing
+
+import typing_extensions
+
+
+def _monkey_patch_typing():
+    if sys.version_info < (3, 8):
+        for extension in [
+            "Literal",
+            "Protocol",
+            "get_args",
+            "get_origin",
+            "runtime_checkable",
+        ]:
+            _extend_typing(extension)
+
+
+def _extend_typing(attr: str) -> None:
+    extension = getattr(typing_extensions, attr)
+    if getattr(typing, attr, None) is not extension:
+        setattr(typing, attr, extension)
+
+
+_monkey_patch_typing()
