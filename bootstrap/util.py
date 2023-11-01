57efb984c045fe5028d18f7df2bd331edf4b8be2
ijknabla <ijknabla@gmail.com>
2023-11-01 14:34:36 +0900

    Implement ensure_cancel

--- bootstrap/util.py
+++ bootstrap/util.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+from asyncio import CancelledError, Task
 from asyncio.subprocess import Process
 from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
+from contextlib import asynccontextmanager, suppress
+from typing import TypeVar
+
+_T = TypeVar("_T")
 
 
 @asynccontextmanager
@@ -15,3 +19,13 @@ async def aterminating(
         if process.returncode is None:
             process.terminate()
         await process.wait()
+
+
+@asynccontextmanager
+async def ensure_cancel(task: Task[_T]) -> AsyncGenerator[Task[_T], None]:
+    try:
+        yield task
+    finally:
+        task.cancel()
+        with suppress(CancelledError):
+            await task
