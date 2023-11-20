c3d5f88d45f00888b43e67397789fec618c553d9
ijknabla <ijknabla@gmail.com>
2023-11-21 00:28:55 +0900

    Deprecate bind_to_awaitable

--- omc4py/algorithm.py
+++ omc4py/algorithm.py
@@ -1,31 +1,10 @@
 from __future__ import annotations
 
-from collections.abc import Awaitable, Callable, Coroutine
-from functools import wraps
-from typing import Any, TypeVar
+from collections.abc import Callable, Coroutine
+from typing import TypeVar
 
 T_a = TypeVar("T_a")
 T_b = TypeVar("T_b")
-
-
-def bind_to_awaitable(
-    f: Callable[[T_a], T_b]
-) -> (
-    Callable[[T_a], T_b] | Callable[[Awaitable[T_a]], Coroutine[Any, Any, T_b]]
-):
-    @wraps(f)
-    def wrapped(x: T_a | Awaitable[T_a]) -> T_b | Coroutine[Any, Any, T_b]:
-        if isinstance(x, Awaitable):
-
-            @wraps(f)
-            async def bind(x: Awaitable[T_a]) -> T_b:
-                return f(await x)
-
-            return bind(x)
-        else:
-            return f(x)
-
-    return wrapped  # type: ignore
 
 
 def fmap(
