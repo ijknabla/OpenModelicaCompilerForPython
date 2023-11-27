dfa3179ae8ca9d3fd96cdf14dae8d38caa0d2f70
ijknabla <ijknabla@gmail.com>
2023-11-27 21:24:22 +0900

    Add installPackage

--- .github/pretest.py
+++ .github/pretest.py
@@ -1,8 +1,13 @@
 from asyncio import run
 
+from omc4py import open_session
+
 
 async def main() -> None:
-    ...
+    with open_session(asyncio=True) as session:
+        if not await session.loadModel("Modelica"):
+            if hasattr(session, "installPackage"):
+                assert await session.installPackage("Modelica")
 
 
 if __name__ == "__main__":
