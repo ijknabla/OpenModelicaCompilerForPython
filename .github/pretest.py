be9b9c7e15e7bdf1c81c476d2523a4624f1f733c
ijknabla <ijknabla@gmail.com>
2023-11-27 21:35:57 +0900

    Add omlib-modelica install

--- .github/pretest.py
+++ .github/pretest.py
@@ -1,5 +1,6 @@
+import asyncio
 import logging
-from asyncio import run
+from subprocess import CalledProcessError, run
 
 from omc4py import open_session
 
@@ -9,10 +10,26 @@ async def main() -> None:
         if not await session.loadModel("Modelica"):
             if hasattr(session, "installPackage"):
                 await session.installPackage("Modelica")
+        if not await session.loadModel("Modelica"):
+            run(["sudo", "apt", "update"], check=True)
+            for version in ["4.0.0", "3.2.3", "3.2.2"]:
+                try:
+                    run(
+                        [
+                            "sudo",
+                            "apt",
+                            "install",
+                            "-qy",
+                            f"omlib-modelica-{version}",
+                        ],
+                        check=True,
+                    )
+                except CalledProcessError:
+                    continue
 
 
 if __name__ == "__main__":
     logging.root.setLevel(logging.DEBUG)
     logging.getLogger("omc4py").addHandler(logging.StreamHandler())
 
-    run(main())
+    asyncio.run(main())
