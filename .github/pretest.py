df7a3e3c7bd38889e2c5fc431a1093fd3f79d764
ijknabla <ijknabla@gmail.com>
2023-11-28 00:47:22 +0900

    Fix versions

--- .github/pretest.py
+++ .github/pretest.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import asyncio
 import logging
+from collections.abc import Iterable
 from contextlib import suppress
 from subprocess import CalledProcessError, run
 
@@ -8,22 +11,20 @@ from omc4py import AsyncSession, open_session
 
 async def main() -> None:
     with open_session(asyncio=True) as session:
-        assert await ensure_package(session, "Modelica", "3.2.2")
+        assert await ensure_package(session, "Modelica")
         assert await session.loadModel("Modelica")
 
 
 async def ensure_package(
     session: AsyncSession,
     pkg: str,
-    version: str,
+    versions: Iterable[str] = ("4.0.0", "3.2.3", "3.2.2"),
 ) -> bool:
     if hasattr(session, "installPackage"):
-        return await session.installPackage(
-            pkg=pkg, version=version, exactMatch=True
-        )
+        return await session.installPackage(pkg=pkg)
 
     run(["sudo", "apt", "update"], check=True)
-    for version in ["4.0.0", "3.2.3", "3.2.2"]:
+    for version in versions:
         with suppress(CalledProcessError):
             run(
                 [
