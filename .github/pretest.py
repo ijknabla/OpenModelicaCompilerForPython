a4b4210c238da14442f2edd9a3ba1c92f7b0e305
ijknabla <ijknabla@gmail.com>
2023-11-27 21:27:23 +0900

    Add logger to omc4py

--- .github/pretest.py
+++ .github/pretest.py
@@ -1,3 +1,4 @@
+import logging
 from asyncio import run
 
 from omc4py import open_session
@@ -7,8 +8,11 @@ async def main() -> None:
     with open_session(asyncio=True) as session:
         if not await session.loadModel("Modelica"):
             if hasattr(session, "installPackage"):
-                assert await session.installPackage("Modelica")
+                await session.installPackage("Modelica")
 
 
 if __name__ == "__main__":
+    logging.root.setLevel(logging.DEBUG)
+    logging.getLogger("omc4py").addHandler(logging.StreamHandler())
+
     run(main())
