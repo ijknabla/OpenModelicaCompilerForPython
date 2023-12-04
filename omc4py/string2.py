5fd101dc5fc727b7250a5521ff2ff3c4af4285e9
ijknabla <ijknabla@gmail.com>
2023-12-04 22:41:22 +0900

    Implement _is_none

---
+++ omc4py/string2.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from contextlib import suppress
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from typing import _SpecialForm
+
+    from typing_extensions import TypeAlias, TypeGuard
+
+SupportedType: TypeAlias = None
+
+
+def _is_none(obj: Any) -> TypeGuard[None]:
+    return obj is None or _issubclass(obj, (type(None),))
+
+
+def _issubclass(
+    obj: Any, class_: tuple[type[Any] | _SpecialForm, ...], /
+) -> bool:
+    if obj in class_:
+        return True
+    with suppress(TypeError):
+        return isinstance(obj, type) and issubclass(
+            obj, tuple(c for c in class_ if isinstance(c, type))
+        )
+    return False
