71ddf3de226fc4372632392212de3a8ec85850b3
ijknabla <ijknabla@gmail.com>
2023-12-05 13:07:10 +0900

    Implement test for path-like

--- tests/test_string2.py
+++ tests/test_string2.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 import types
 from collections.abc import Generator
 from contextlib import suppress
@@ -7,7 +8,14 @@ from typing import Any, Literal, NamedTuple, Union
 
 import pytest
 
-from omc4py.string2 import _is_literal, _is_none, _is_union, _StringableType
+import omc4py.protocol
+from omc4py.string2 import (
+    _is_literal,
+    _is_none,
+    _is_path_like,
+    _is_union,
+    _StringableType,
+)
 
 
 class TestCase(NamedTuple):
@@ -17,6 +25,7 @@ class TestCase(NamedTuple):
     is_none: bool = False
     is_literal: bool = False
     is_union: bool = False
+    is_path_like: bool = False
 
 
 def _iter_test_cases() -> Generator[TestCase, None, None]:
@@ -64,6 +73,29 @@ def _iter_test_cases() -> Generator[TestCase, None, None]:
             is_union=True,
         )
 
+    # PathLike
+    yield TestCase(
+        annotation=omc4py.protocol.PathLike,
+        type=str,
+        is_path_like=True,
+    )
+    yield TestCase(
+        annotation=omc4py.protocol.PathLike[str],
+        type=str,
+        is_path_like=True,
+    )
+    yield TestCase(
+        annotation=os.PathLike,
+        type=str,
+        is_path_like=True,
+    )
+    with suppress(TypeError):
+        yield TestCase(
+            annotation=os.PathLike[str],
+            type=str,
+            is_path_like=True,
+        )
+
 
 @pytest.mark.parametrize("test_case", _iter_test_cases())
 def test_annotation_checker(test_case: TestCase) -> None:
@@ -71,3 +103,4 @@ def test_annotation_checker(test_case: TestCase) -> None:
     assert _is_none(x.annotation) == x.is_none
     assert _is_literal(x.annotation) == x.is_literal
     assert _is_union(x.annotation) == x.is_union
+    assert _is_path_like(x.annotation) == x.is_path_like
