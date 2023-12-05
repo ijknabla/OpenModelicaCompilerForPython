22a53b097d678ac60bcceae1c834a1c9e93f42e9
ijknabla <ijknabla@gmail.com>
2023-12-05 11:57:58 +0900

    Add _is_literal

--- omc4py/string2.py
+++ omc4py/string2.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from contextlib import suppress
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Literal, get_origin
 
 if TYPE_CHECKING:
     from typing import _SpecialForm
@@ -13,6 +13,10 @@ SupportedType: TypeAlias = None
 
 def _is_none(obj: Any) -> TypeGuard[None]:
     return obj is None or _issubclass(obj, (type(None),))
+
+
+def _is_literal(obj: Any) -> bool:
+    return _issubclass(get_origin(obj), (Literal,))
 
 
 def _issubclass(
