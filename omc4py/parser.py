5cf59a9966018cb0a0cce8d1ee9da2940cea74cd
ijknabla <ijknabla@gmail.com>
2023-12-26 00:02:34 +0900

    Remove unused functions

--- omc4py/parser.py
+++ omc4py/parser.py
@@ -1,10 +1,6 @@
 from __future__ import annotations
 
-import enum
-import sys
-from collections.abc import Callable, Mapping, Sequence
-from functools import lru_cache, wraps
-from itertools import chain, islice
+from functools import lru_cache
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -13,11 +9,8 @@ from typing import (
     Literal,
     Protocol,
     Tuple,
-    TypeVar,
     Union,
 )
-from typing import cast as typing_cast
-from typing import get_args, get_origin, get_type_hints
 
 from arpeggio import (
     EOF,
@@ -33,49 +26,11 @@ from arpeggio import (
     visit_parse_tree,
 )
 from modelicalang import ParsingExpressionLike, v3_4
-from typing_extensions import Annotated
 
-from omc4py.protocol import PathLike
-
-from .exception import OMCRuntimeError
-from .modelica import enumeration, record
-from .openmodelica import Component, TypeName, VariableName
 from .string import unquote_modelica_string
 
 if TYPE_CHECKING:
-    import typing
-    from builtins import _ClassInfo
-
-    import typing_extensions
-    from typing_extensions import Concatenate, Never, ParamSpec, TypeGuard
-
-_SpecialForm = Union[
-    "typing._SpecialForm",
-    "typing_extensions._SpecialForm",
-    "type[PathLike]",
-]
-
-
-_T = TypeVar("_T")
-_T_return = TypeVar("_T_return")
-
-if TYPE_CHECKING:
-    _P = ParamSpec("_P")
-else:
-    _P = ...
-
-_Scalar = Union[
-    float,
-    int,
-    bool,
-    PathLike,
-    str,
-    VariableName,
-    TypeName,
-    Component,
-    enumeration,
-    record,
-]
+    from typing_extensions import Never
 
 
 def is_variablename(variablename: str) -> bool:
@@ -94,67 +49,6 @@ def split_typename_parts(typename: str) -> tuple[str, ...]:
     )
 
 
-def parse(typ: type[_T], literal: str) -> _T:
-    parser = _get_parser(
-        *map(_Token.from_value_type, _get_types(typ, keep_component=True))
-    )
-    try:
-        parse_tree = parser.parse(literal)
-    except NoMatch:
-        raise OMCRuntimeError(literal) from None
-    return cast(typ, visit_parse_tree(parse_tree, Visitor()))
-
-
-def cast(typ: type[_T], val: Any) -> _T:
-    cast_type = _get_cast_type(typ)
-    if _issubclass(cast_type, tuple):
-        return _cast_tuple(val, cast_type, typ)  # type: ignore
-    elif _is_enumeration_type(cast_type):
-        return _cast_enumeration(val, cast_type, typ)
-    elif _is_record_type(cast_type):
-        return _cast_record(val, cast_type, typ)
-    else:
-        return _cast_value(val, cast_type, typ)
-
-
-class _Token(enum.Enum):
-    real = enum.auto()
-    integer = enum.auto()
-    boolean = enum.auto()
-    string = enum.auto()
-    variablename = enum.auto()
-    typename = enum.auto()
-    component = enum.auto()
-    record = enum.auto()
-
-    @property
-    def primary(self) -> ParsingExpressionLike:
-        return getattr(Syntax, f"{self.name}_primary")  # type: ignore
-
-    @classmethod
-    def from_value_type(
-        cls, value_type: type[_Scalar] | type[Component]
-    ) -> _Token:
-        if issubclass(value_type, float):
-            return cls.real
-        elif not issubclass(value_type, bool) and issubclass(value_type, int):
-            return cls.integer
-        elif issubclass(value_type, bool):
-            return cls.boolean
-        elif issubclass(value_type, str):
-            return cls.string
-        elif issubclass(value_type, VariableName):
-            return cls.variablename
-        elif issubclass(value_type, (TypeName, enumeration)):
-            return cls.typename
-        elif issubclass(value_type, Component):
-            return cls.component
-        elif issubclass(value_type, record):
-            return cls.record
-
-        raise NotImplementedError(value_type)
-
-
 @lru_cache(1)
 def _get_variablename_parser() -> ParserPython:
     def root() -> ParsingExpressionLike:
@@ -171,228 +65,6 @@ def _get_typename_parser() -> ParserPython:
 
     with Syntax:
         return ParserPython(root)
-
-
-@lru_cache(None)
-def _get_parser(*tokens: _Token) -> ParserPython:
-    def root() -> ParsingExpressionLike:
-        if len(tokens) < 2:
-            L = R = []
-        else:
-            L, R = ["("], [")"]
-
-        return (*L, primary_list, *R, EOF)
-
-    def primary_list() -> ParsingExpressionLike:
-        primaries = (token.primary for token in tokens)
-        return (
-            *islice(primaries, 1),
-            *chain.from_iterable([",", primary] for primary in primaries),
-        )
-
-    with Syntax:
-        return ParserPython(root)
-
-
-def _vectorize(
-    f: Callable[Concatenate[_T, _P], _T_return]
-) -> Callable[Concatenate[_T, _P], _T_return]:
-    @wraps(f)
-    def wrapped(_: _T, *args: _P.args, **kwargs: _P.kwargs) -> Any:
-        val = _
-        if isinstance(val, Sequence) and not isinstance(
-            val, (str, bytes, tuple)
-        ):
-            return [wrapped(i, *args, **kwargs) for i in val]
-        else:
-            return f(val, *args, **kwargs)  # type: ignore
-
-    return wrapped  # type: ignore
-
-
-@_vectorize
-def _cast_tuple(val: Any, typ: type[tuple[Any, ...]], hint: type[_T]) -> _T:
-    def items() -> Iterable[Any]:
-        for item, item_typ in zip(val, _get_types(typ, keep_component=False)):
-            yield cast(item_typ, item)
-
-    return typing_cast(_T, typ(*items()))
-
-
-@_vectorize
-def _cast_enumeration(val: Any, typ: type[enumeration], hint: type[_T]) -> _T:
-    if isinstance(val, typ):
-        return typing_cast(_T, val)
-    elif isinstance(val, int):
-        return typing_cast(_T, typ(val))
-    elif isinstance(val, str):
-        typename = TypeName(val).as_absolute()
-        if typename.parent == typ.__omc_class__.as_absolute():
-            name = f"{typename.last_identifier}"
-        else:
-            name = val
-        return typing_cast(_T, typ[name])
-
-    raise TypeError(val)
-
-
-@_vectorize
-def _cast_record(val: Any, typ: type[record], hint: type[_T]) -> _T:
-    if isinstance(val, typ):
-        return typing_cast(_T, val)
-    elif isinstance(val, Mapping):
-        type_hints = get_type_hints(typ)
-
-        # Patch for bug at `OpenModelica.Scripting.getMessagesStringInternal`
-        if typ.__omc_class__ == TypeName("OpenModelica.Scripting.SourceInfo"):
-            if "fileName" in type_hints and "filename" in val:
-                val = {
-                    k if k != "filename" else "fileName": v
-                    for k, v in val.items()
-                }
-
-        return typing_cast(
-            _T, typ(**{k: cast(type_hints[k], v) for k, v in val.items()})
-        )
-
-    raise TypeError(val)
-
-
-@_vectorize
-def _cast_value(val: Any, typ: Any, hint: type[_T]) -> _T:
-    if not isinstance(val, typ):
-        val = typ(val)
-
-    return typing_cast(_T, val)
-
-
-def _get_types(typ: Any, *, keep_component: bool) -> tuple[type[_Scalar], ...]:
-    if _is_none(typ):
-        return ()
-    elif _issubclass(typ, Component) and keep_component:
-        return (typ,)
-    elif _issubclass(typ, tuple):
-        return tuple(map(_get_type, get_type_hints(typ).values()))
-    else:
-        return (_get_type(typ),)
-
-
-def _get_type(typ: Any) -> type[_Scalar]:
-    if _isorigin(typ, Annotated):
-        arg, *_ = get_args(typ)
-        return _get_type(arg)
-    elif _issubclass(get_origin(typ), Sequence):
-        (arg,) = get_args(typ)
-        return _get_type(arg)
-    elif _is_union(typ):
-        return _select_type(map(_get_type, get_args(typ)))
-    elif get_origin(typ) is Literal:
-        return _select_type(map(type, get_args(typ)))
-    elif _issubclass(typ, get_args(_Scalar)):
-        return typ  # type: ignore
-    else:
-        raise NotImplementedError(typ)
-
-
-def _get_cast_type(typ: Any) -> type[None | _Scalar | tuple[Any, ...]]:
-    if _is_none(typ):
-        return type(None)
-    elif _isorigin(typ, Annotated):
-        arg, *_ = get_args(typ)
-        return _get_cast_type(arg)
-    elif _issubclass(typ, tuple):
-        return typ  # type: ignore
-    elif _issubclass(get_origin(typ), Sequence):
-        (arg,) = get_args(typ)
-        return _get_cast_type(arg)
-    elif _is_union(typ):
-        return _select_type(map(_get_cast_type, get_args(typ)))
-    elif _isorigin(typ, Literal):
-        return _select_type(map(_get_cast_type, map(type, get_args(typ))))
-    elif _is_path_like(typ):
-        return PathLike
-    elif _issubclass(typ, get_args(_Scalar)):
-        return typ  # type: ignore
-    else:
-        raise NotImplementedError(typ)
-
-
-def _is_union(typ: Any) -> bool:
-    if _isorigin(typ, Union):
-        return True
-    if sys.version_info >= (3, 10):
-        from types import UnionType
-
-        if isinstance(typ, UnionType):
-            return True
-
-    return False
-
-
-def _is_path_like(typ: Any) -> bool:
-    import os
-
-    if _isorigin(typ, PathLike) or _isorigin(typ, os.PathLike):
-        return True
-    if _issubclass(typ, PathLike) or _issubclass(typ, os.PathLike):
-        return True
-    return False
-
-
-def _select_type(typs: Iterable[type[Any]]) -> type[Any]:
-    return max(typs, key=_priority)
-
-
-def _priority(typ: type[_Scalar]) -> int:
-    if _is_none(typ):
-        return 0
-    for i, base in reversed(
-        list(
-            enumerate(
-                [
-                    float,
-                    int,
-                    PathLike,
-                    str,
-                    VariableName,
-                    TypeName,
-                    enumeration,
-                    record,
-                ],
-                start=1,
-            )
-        )
-    ):
-        if _issubclass(typ, base):
-            return i
-
-    raise NotImplementedError(typ)
-
-
-def _is_none(
-    typ: Any,
-) -> TypeGuard[type[None] | None]:
-    return typ is None or _issubclass(typ, type(None))
-
-
-def _is_enumeration_type(
-    typ: Any,
-) -> TypeGuard[type[enumeration]]:
-    return isinstance(typ, type) and issubclass(typ, enumeration)
-
-
-def _is_record_type(
-    typ: Any,
-) -> TypeGuard[type[record]]:
-    return isinstance(typ, type) and issubclass(typ, record)
-
-
-def _issubclass(__cls: Any, __class_or_tuple: _ClassInfo) -> bool:
-    return isinstance(__cls, type) and issubclass(__cls, __class_or_tuple)
-
-
-def _isorigin(__cls: Any, __special_form: _SpecialForm) -> bool:
-    return get_origin(__cls) is __special_form
 
 
 class Syntax(v3_4.Syntax):
