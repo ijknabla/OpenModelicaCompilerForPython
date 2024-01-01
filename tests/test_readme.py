9a3acef362a63b8d461e3b32e18643ca17fbf161
ijknabla <ijknabla@gmail.com>
2024-01-01 14:29:30 +0900

    Implement _remove_prompt

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -9,10 +9,10 @@ import pytest
     [],
 )
 def test_readme(source: str) -> None:
-    for f in [_unquote, _replace_omc_to_none]:
+    for f in [_unquote, _replace_omc_to_none, _remove_prompt]:
         source = f(source)
 
-    exec(source)
+    exec(source, {"exit": lambda: None})
 
 
 def _unquote(s: str, /) -> str:
@@ -37,3 +37,16 @@ def _replace_omc_to_none(s: str, /) -> str:
     else:
         pattern = "(" + "|".join(map(re.escape, sorted(missing_omc_exe))) + ")"
         return re.sub("|".join(q + pattern + q for q in ['"', "'"]), "None", s)
+
+
+def _remove_prompt(s: str, /) -> str:
+    lines = list(
+        map(
+            lambda m: m.group("line"),
+            re.finditer(r"^(>>>|\.\.\.) (?P<line>.*\n)", s, re.MULTILINE),
+        )
+    )
+    if not lines:
+        return s
+    else:
+        return "".join(lines)
