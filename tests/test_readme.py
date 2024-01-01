5c5c55fcdc73153f4b4003274fe99ef85dc6e94f
ijknabla <ijknabla@gmail.com>
2024-01-01 15:18:34 +0900

    Add source

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -6,7 +6,154 @@ import pytest
 
 @pytest.mark.parametrize(
     "source,",
-    [],
+    [
+        """
+```python3
+#!/usr/bin/env python3
+import omc4py
+
+with omc4py.open_session() as session:
+    print(session.getVersion())
+```
+        """,
+        """
+```python3
+import omc4py
+
+with omc4py.open_session(
+    "C:/OpenModelica1.13.0-64bit/bin/omc.exe"
+) as session:
+    print(session.getVersion())
+```
+        """,
+        """
+```python3
+import omc4py
+
+with \
+    omc4py.open_session(
+        "C:/OpenModelica1.13.0-64bit/bin/omc.exe"
+    ) as session_13, \
+    omc4py.open_session(
+        "C:/Program Files/OpenModelica1.14.0-64bit/bin/omc.exe"
+    ) as session_14:
+
+    print("v1.13.0:", session_13.getVersion())
+    print("v1.14.0:", session_14.getVersion())
+```
+        """,
+        '''
+```python3
+>>> from omc4py import *
+>>> session = open_session()
+>>> session.loadString("""
+... package A
+...     package B
+...             package C
+...             end C;
+...     end B;
+... end A;
+... """)
+True
+>>> list(session.getClassNames("A", recursive=True))
+[TypeName('A'), TypeName('A.B'), TypeName('A.B.C')]
+>>>
+>>>
+>>> exit()  # session will be closed internally
+```
+        ''',
+        """
+```python3
+>>> from omc4py import *
+>>> session = open_session()
+>>> session.__close__()
+>>>
+>>> exit()
+```
+        """,
+        """
+```python3
+# Example for "timerTick" and "timerTock"
+# in "OpenModelica.Scripting.Internal.Time"
+from omc4py import open_session
+from time import sleep
+
+timer_index: int = 1
+
+with open_session() as session:
+    session.OpenModelica.Scripting.Internal.Time.timerTick(timer_index)
+
+    sleep(0.1)
+
+    # show elapsed time from last timerTick
+    print(session.OpenModelica.Scripting.Internal.Time.timerTock(timer_index))
+```
+        """,
+        """
+```python3
+import omc4py
+
+with omc4py.open_session() as session:
+    assert(session.loadModel("Modelica"))  # load MSL
+```
+        """,
+        """
+```python3
+import omc4py
+
+with omc4py.open_session() as session:
+    assert(session.loadModel("Modelica", ["3.2.3"]))  # load MSL 3.2.3
+```
+        """,
+        """
+```python3
+import omc4py
+
+with omc4py.open_session() as session:
+    assert(session.loadModel("Modelica"))
+    for className in session.getClassNames("Modelica"):
+        print(className)
+```
+        """,
+        """
+```python3
+import omc4py
+
+with omc4py.open_session() as session:
+    assert(session.loadModel("Modelica"))
+    for className in session.getClassNames("Modelica", recursive=True):
+        print(className)  # many class names will be printed
+```
+        """,
+        """
+```python3
+import omc4py
+
+with omc4py.open_session() as session:
+    assert(session.loadModel("Modelica", ["3.2.3"]))
+    for component in session.getComponents("Modelica.Constants"):
+        print(
+            f"{component.className.last_identifier!s:<20}"
+            f"{component.name!s:<15}"
+            f"{component.comment!r}"
+        )
+```
+        """,
+        """
+```python3
+from omc4py import open_session
+
+def doubtful_task(session):
+    # session.doubtful_API1(...)
+    # session.doubtful_API2(...)
+    # session.doubtful_API3(...)
+    session.__check__()
+
+with open_session() as session:
+    doubtful_task(session)
+```
+        """,
+    ],
 )
 def test_readme(source: str) -> None:
     for f in [_unquote, _replace_omc_to_none, _remove_prompt]:
